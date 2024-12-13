1b033674a9d81c2890a53f41c218f25413ced323
Maksym <zzzeleznyakmaxim@gmail.com>
2024-12-13 20:06:52 +0200

    mobilemenu

---
+++ src/script.js
@@ -0,0 +1,17 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[mobile-menu]'),
+    closeModalBtn: document.querySelector('[close-modal-btn]'),
+    modal: document.querySelector('[modal-menu]'),
+  };
+
+  // Открытие модального окна
+  refs.openModalBtn.addEventListener('click', () => {
+    refs.modal.classList.add('is-open');
+  });
+
+  // Закрытие модального окна
+  refs.closeModalBtn.addEventListener('click', () => {
+    refs.modal.classList.remove('is-open');
+  });
+})();
