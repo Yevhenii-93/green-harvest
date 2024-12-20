32215125d634cf5caa17f5aab757023aa62c0d47
Mykhajlo-Berezhnyj <bmve@ukr.net>
2024-12-20 05:54:40 +0200

    add effect&fix

---
+++ src/js/script-paused.js
@@ -0,0 +1,7 @@
+function pauseAnimation() {
+  document.querySelector('.slider').classList.add('paused');
+}
+
+function resumeAnimation() {
+  document.querySelector('.slider').classList.remove('paused');
+}
